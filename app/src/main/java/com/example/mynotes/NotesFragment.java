18a59d3c1c1ce8fdc2593cfbec817b5dad0e1e76
vikzak <victor@azbukait.ru>
2021-12-14 11:13:09 +0300

    ДЗ№6-add

---
+++ app/src/main/java/com/example/mynotes/NotesFragment.java
@@ -0,0 +1,110 @@
+package com.example.mynotes;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link NotesFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class NotesFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+//    private static final String ARG_PARAM1 = "param1";
+//    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+//    private String mParam1;
+//    private String mParam2;
+
+//    public NotesFragment() {
+//        // Required empty public constructor
+//    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment NotesFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+//    public static NotesFragment newInstance(String param1, String param2) {
+//        NotesFragment fragment = new NotesFragment();
+//        Bundle args = new Bundle();
+//        args.putString(ARG_PARAM1, param1);
+//        args.putString(ARG_PARAM2, param2);
+//        fragment.setArguments(args);
+//        return fragment;
+//    }
+
+    /*@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }*/
+
+    // При создании фрагмента укажем его макет
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_notes, container, false);
+    }
+
+    // Этот метод вызывается, когда макет экрана создан и готов к отображению информации.
+    // Создаем список заметок
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        initList(view);
+    }
+
+
+    // создаём список городов на экране из массива в ресурсах
+    private void initList(View view) {
+        LinearLayout layoutView = (LinearLayout) view;
+        String[] notes_name = getResources().getStringArray(R.array.notes_name);
+        // В этом цикле создаём элемент TextView, // заполняем его значениями,
+        // и добавляем на экран.
+        for (int i = 0; i < notes_name.length; i++) {
+            String city = notes_name[i];
+            TextView tv = new TextView(getContext());
+            tv.setText(city);
+            tv.setTextSize(30);
+            layoutView.addView(tv);
+            final int position = i;
+            tv.setOnClickListener(v -> {
+                showCoatOfArms(position);
+            });
+        }
+
+    }
+
+    private void showCoatOfArms(int index) {
+        CoatOfArmsFragment coatOfArmsFragment =
+                CoatOfArmsFragment.newInstance(index);
+        FragmentManager fragmentManager =
+                requireActivity().getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction =
+                fragmentManager.beginTransaction();
+        // добавляем фрагмент через add
+        fragmentTransaction.add(R.id.fragment_container, coatOfArmsFragment);
+        fragmentTransaction.commit();
+    }
+}
