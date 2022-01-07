537545bedf9308a9a3ab2b4949abca3e4f4abefc
vikzak <victor@azbukait.ru>
2022-01-07 19:18:18 +0300

    ДЗ№7 - добавил протопип интерфейса кнопок.

--- app/src/main/java/com/example/mynotes/MainActivity.java
+++ app/src/main/java/com/example/mynotes/MainActivity.java
@@ -3,10 +3,13 @@ package com.example.mynotes;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Context;
 import android.content.res.Configuration;
 import android.os.Bundle;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,6 +23,28 @@ public class MainActivity extends AppCompatActivity {
                 .addToBackStack("")
                 .add(R.id.fragment_container, new NotesFragment())
                 .commit();
+
+        findViewById(R.id.button_add).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"add notes. Скоро...", Toast.LENGTH_SHORT).show();
+            }
+
+        });
+
+        findViewById(R.id.button_options).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Options fregment. Скоро...", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        findViewById(R.id.button_del).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Delete notes. Скоро...", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
 
